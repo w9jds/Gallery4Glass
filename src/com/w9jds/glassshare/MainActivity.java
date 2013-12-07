87e9154042b65fe995968cbfcadcbe2bd7bc8799
Jeremy Shore <xboxmusicn3rd@gmail.com>
2013-12-07 00:49:21 -0600

    Delete now works, but there are still issues with performance. Need to figure out why it runs like crap.

--- src/com/w9jds/glassshare/MainActivity.java
+++ src/com/w9jds/glassshare/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import com.google.android.glass.widget.CardScrollView;
 import com.w9jds.glassshare.Adapters.csaAdapter;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -13,6 +14,7 @@ import android.provider.MediaStore.Images;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
@@ -25,32 +27,42 @@ public class MainActivity extends Activity
 	public static final String CAMERA_IMAGE_BUCKET_NAME = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + "/Camera";
 	public static final String CAMERA_IMAGE_BUCKET_ID = getBucketId(CAMERA_IMAGE_BUCKET_NAME);
 	
+	//custom adapter
+	private csaAdapter mcvAdapter;
+	//list for all the paths of the images on google glass
 	private ArrayList<String> mlsPaths = new ArrayList<String>();
+	//variable for the last selected index
 	private int iPosition;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-			
+		
+		//get all the images from the camera folder (paths)
 		mlsPaths = getCameraImages(this);
-		
+		//create a new card scroll viewer for this context
 		CardScrollView csvCardsView = new CardScrollView(this);
-
-		csaAdapter cvAdapter = new csaAdapter(this, mlsPaths);
-		csvCardsView.setAdapter(cvAdapter);
+		//create a new adapter for the scroll viewer
+		mcvAdapter = new csaAdapter(this, mlsPaths);
+		//set this adapter as the adapter for the scroll viewer
+		csvCardsView.setAdapter(mcvAdapter);
+		//activate this scroll viewer
 		csvCardsView.activate();
-		
+		//add a listener to the scroll viewer that is fired when an item is clicked
         csvCardsView.setOnItemClickListener(new OnItemClickListener() 
         {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
 			{
+				//save the card index that was selected
 				iPosition = position;
+				//open the menu
 				openOptionsMenu();
 			}
         });
 		
+        //set the view of this activity
 		setContentView(csvCardsView);
 	}
 	
@@ -95,8 +107,17 @@ public class MainActivity extends Activity
 		switch (item.getItemId()) 
 		{
 	        case R.id.delete_menu_item:
-//	        	File fPic = new File(mlsPaths.get(iPosition));
-//	        	fPic.delete();
+	        	//pull the file from the path of the selected item
+	        	File fPic = new File(mlsPaths.get(iPosition));
+	        	//delete the image
+	        	fPic.delete();
+	        	//refresh the folder
+	        	sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" +  Environment.getExternalStorageDirectory())));
+	        	//remove the selected item from the list of images
+	        	mlsPaths.remove(iPosition);
+	        	//let the adapter know that the list of images has changed
+	        	mcvAdapter.notifyDataSetChanged();
+	        	//handled
 	            return true;
 	        case R.id.share_menu_item:
 	        	return true;
@@ -104,5 +125,8 @@ public class MainActivity extends Activity
 	            return super.onOptionsItemSelected(item);
 		}
 	};
+
 }
+
+
 
