f6c69e75bc44829afd1fd4146f6292c95a435afc
Jeremy Shore <xboxmusicn3rd@gmail.com>
2013-12-14 00:34:13 -0600

    Gradle update.

--- GlassShare/src/main/java/com/w9jds/glassshare/Adapters/csaAdapter.java
+++ GlassShare/src/main/java/com/w9jds/glassshare/Adapters/csaAdapter.java
@@ -21,46 +21,39 @@ import java.util.ArrayList;
 /**
  * Created by w9jds on 12/13/13.
  */
-public class csaAdapter extends CardScrollAdapter
-{
+public class csaAdapter extends CardScrollAdapter {
     private Context mcContext;
     private ArrayList<String> mlsPaths;
     private Bitmap mPlaceHolderBitmap;
 
-    public csaAdapter(Context cContext, ArrayList<String> alsPaths)
-    {
+    public csaAdapter(Context cContext, ArrayList<String> alsPaths) {
         mcContext = cContext;
         mlsPaths = alsPaths;
         mPlaceHolderBitmap = BitmapFactory.decodeResource(mcContext.getResources(), R.drawable.ic_placeholder_photo_150);
     }
 
     @Override
-    public int findIdPosition(Object id)
-    {
+    public int findIdPosition(Object id) {
         return -1;
     }
 
     @Override
-    public int findItemPosition(Object item)
-    {
+    public int findItemPosition(Object item) {
         return mlsPaths.indexOf(item);
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         return mlsPaths.size();
     }
 
     @Override
-    public Object getItem(int position)
-    {
+    public Object getItem(int position) {
         return mlsPaths.get(position);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) mcContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View vCard = inflater.inflate(R.layout.card_layout, parent, false);
 
@@ -68,8 +61,7 @@ public class csaAdapter extends CardScrollAdapter
         ImageView ivPic = (ImageView) vCard.findViewById(R.id.cardImage);
 
 
-        if (cancelPotentialWork(position, ivPic))
-        {
+        if (cancelPotentialWork(position, ivPic)) {
             final BitmapWorkerTask task = new BitmapWorkerTask(ivPic, mlsPaths, position);
 
             final AsyncDrawable asyncDrawable = new AsyncDrawable(mcContext.getResources(), mPlaceHolderBitmap, task);
@@ -82,12 +74,10 @@ public class csaAdapter extends CardScrollAdapter
         return vCard;
     }
 
-    public static boolean cancelPotentialWork(int iPosition, ImageView ivCard)
-    {
+    public static boolean cancelPotentialWork(int iPosition, ImageView ivCard) {
         final BitmapWorkerTask bwtTask = getBitmapWorkerTask(ivCard);
 
-        if (bwtTask != null)
-        {
+        if (bwtTask != null) {
             final int position = bwtTask.miPosition;
             if (position != iPosition)
                 // Cancel previous task
@@ -100,30 +90,24 @@ public class csaAdapter extends CardScrollAdapter
         return true;
     }
 
-    static class AsyncDrawable extends BitmapDrawable
-    {
+    static class AsyncDrawable extends BitmapDrawable {
         private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
 
-        public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask)
-        {
+        public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
             super(res, bitmap);
-            bitmapWorkerTaskReference =  new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
+            bitmapWorkerTaskReference = new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
         }
 
-        public BitmapWorkerTask getBitmapWorkerTask()
-        {
+        public BitmapWorkerTask getBitmapWorkerTask() {
             return bitmapWorkerTaskReference.get();
         }
     }
 
 
-    private static BitmapWorkerTask getBitmapWorkerTask(ImageView ivCard)
-    {
-        if (ivCard != null)
-        {
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView ivCard) {
+        if (ivCard != null) {
             final Drawable drawable = ivCard.getDrawable();
-            if (drawable instanceof AsyncDrawable)
-            {
+            if (drawable instanceof AsyncDrawable) {
                 final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
                 return asyncDrawable.getBitmapWorkerTask();
             }
@@ -131,14 +115,12 @@ public class csaAdapter extends CardScrollAdapter
         return null;
     }
 
-    public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap>
-    {
+    public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
         private final ArrayList<String> mlsPaths;
         private final WeakReference<ImageView> mivCard;
         private final int miPosition;
 
-        public BitmapWorkerTask(ImageView ivCard, ArrayList<String> lsPaths, int iPosition)
-        {
+        public BitmapWorkerTask(ImageView ivCard, ArrayList<String> lsPaths, int iPosition) {
             // Use a WeakReference to ensure the ImageView can be garbage collected
             mivCard = new WeakReference<ImageView>(ivCard);
             miPosition = iPosition;
@@ -148,8 +130,7 @@ public class csaAdapter extends CardScrollAdapter
 
         // Decode image in background.
         @Override
-        protected Bitmap doInBackground(Integer... params)
-        {
+        protected Bitmap doInBackground(Integer... params) {
             //create new options for bitmap import
             BitmapFactory.Options bfoOptions = new BitmapFactory.Options();
             //set it so you only get bounds (no pixels)
@@ -157,8 +138,7 @@ public class csaAdapter extends CardScrollAdapter
             //pull the info from the file
             BitmapFactory.decodeFile(mlsPaths.get(miPosition), bfoOptions);
 
-            if (bfoOptions.outWidth > 640)
-            {
+            if (bfoOptions.outWidth > 640) {
                 //turn off the bounds only so you get the pixels this time
                 bfoOptions.inJustDecodeBounds = false;
                 //calculate the sample size and set it in the options
@@ -166,16 +146,14 @@ public class csaAdapter extends CardScrollAdapter
 
                 Bitmap bImage = BitmapFactory.decodeFile(mlsPaths.get(miPosition), bfoOptions);
 
-                double dRatio = ((double)bImage.getWidth()) / 640;
+                double dRatio = ((double) bImage.getWidth()) / 640;
                 return Bitmap.createScaledBitmap(bImage, 640, (int) Math.round(bImage.getHeight() / dRatio), true);
-            }
-            else
+            } else
                 return BitmapFactory.decodeFile(mlsPaths.get(miPosition));
 
         }
 
-        public int calculateInSampleSize( BitmapFactory.Options bfoOptions, int iReqWidth, int iReqHeight)
-        {
+        public int calculateInSampleSize(BitmapFactory.Options bfoOptions, int iReqWidth, int iReqHeight) {
             //pull out the images height
             final int height = bfoOptions.outHeight;
             //pull out the images width
@@ -184,8 +162,7 @@ public class csaAdapter extends CardScrollAdapter
             int inSampleSize = 1;
 
             //if te height or the width of the image is greater than the requested sizes
-            if (height > iReqHeight || width > iReqWidth)
-            {
+            if (height > iReqHeight || width > iReqWidth) {
                 //set the half dimensions for the image
                 final int halfHeight = height / 2;
                 final int halfWidth = width / 2;
@@ -202,17 +179,14 @@ public class csaAdapter extends CardScrollAdapter
 
         // Once complete, see if ImageView is still around and set bitmap.
         @Override
-        protected void onPostExecute(Bitmap bPic)
-        {
+        protected void onPostExecute(Bitmap bPic) {
             if (isCancelled())
                 bPic = null;
 
-            if (mivCard != null && bPic != null)
-            {
+            if (mivCard != null && bPic != null) {
                 final ImageView ivCard = mivCard.get();
                 final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(ivCard);
-                if (this == bitmapWorkerTask && ivCard != null)
-                {
+                if (this == bitmapWorkerTask && ivCard != null) {
                     ivCard.setScaleType(ImageView.ScaleType.CENTER_CROP);
                     ivCard.setImageBitmap(bPic);
                 }
--- GlassShare/src/main/java/com/w9jds/glassshare/MainActivity.java
+++ GlassShare/src/main/java/com/w9jds/glassshare/MainActivity.java
@@ -1,5 +1,7 @@
 package com.w9jds.glassshare;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -10,30 +12,38 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 
 import com.google.android.glass.widget.CardScrollView;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
-import com.newrelic.agent.android.NewRelic;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
 import com.w9jds.glassshare.Adapters.csaAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 @SuppressLint("DefaultLocale")
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     public static final String CAMERA_IMAGE_BUCKET_NAME = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + "/Camera";
     public static final String CAMERA_IMAGE_BUCKET_ID = getBucketId(CAMERA_IMAGE_BUCKET_NAME);
 
     private ConnectivityManager mcmCon;
 
-//    private MobileServiceClient mClient;
-//    private static Drive mdService;
+    //    private MobileServiceClient mClient;
+    private static Drive mdService;
     private GoogleAccountCredential mgacCredential;
 
     //custom adapter
@@ -44,14 +54,16 @@ public class MainActivity extends Activity
     private int iPosition;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mcmCon = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
         if (mcmCon.getActiveNetworkInfo().isConnected())
-            NewRelic.withApplicationToken("AA93aef2e7990a6e523362c247824b8ba5f312109d").start(this.getApplication());
+        {
+//            Parse.initialize(this, "YMdRbwiu0qWNZPSRRoWOxejHDsAQqkZ2MgPomjUC", "ET6wO4zIPHooOMkbk1UXfIV888O9tUdidlu7W6r0");
+//            ParseAnalytics.trackAppOpened(getIntent());
+        }
 
 //        mClient = new MobileServiceClient( "https://glassshare.azure-mobile.net/", "hGZSWGJntAFbXLmYRitDivIhrClsgA21", this );
 
@@ -68,11 +80,9 @@ public class MainActivity extends Activity
         //activate this scroll viewer
         csvCardsView.activate();
         //add a listener to the scroll viewer that is fired when an item is clicked
-        csvCardsView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        csvCardsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //save the card index that was selected
                 iPosition = position;
                 //open the menu
@@ -85,8 +95,7 @@ public class MainActivity extends Activity
 
     }
 
-    private void sortPaths()
-    {
+    private void sortPaths() {
         java.io.File[] fPics = new java.io.File[mlsPaths.size()];
 
         for (int i = 0; i < mlsPaths.size(); i++)
@@ -94,11 +103,9 @@ public class MainActivity extends Activity
 
         mlsPaths.clear();
 
-        Arrays.sort(fPics, new Comparator<java.io.File>()
-        {
+        Arrays.sort(fPics, new Comparator<java.io.File>() {
             @Override
-            public int compare(java.io.File o1, java.io.File o2)
-            {
+            public int compare(java.io.File o1, java.io.File o2) {
                 return Long.valueOf(o1.lastModified()).compareTo(o2.lastModified());
             }
         });
@@ -107,24 +114,20 @@ public class MainActivity extends Activity
             mlsPaths.add(fPics[i].getAbsolutePath());
     }
 
-    public static String getBucketId(String path)
-    {
+    public static String getBucketId(String path) {
         return String.valueOf(path.toLowerCase().hashCode());
     }
 
-    public static ArrayList<String> getCameraImages(Context context)
-    {
-        final String[] projection = { MediaStore.Images.Media.DATA };
+    public static ArrayList<String> getCameraImages(Context context) {
+        final String[] projection = {MediaStore.Images.Media.DATA};
         final String selection = MediaStore.Images.Media.BUCKET_ID + " = ?";
-        final String[] selectionArgs = { CAMERA_IMAGE_BUCKET_ID };
+        final String[] selectionArgs = {CAMERA_IMAGE_BUCKET_ID};
         final Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs, null);
         ArrayList<String> result = new ArrayList<String>(cursor.getCount());
 
-        if (cursor.moveToFirst())
-        {
+        if (cursor.moveToFirst()) {
             final int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            do
-            {
+            do {
                 final String data = cursor.getString(dataColumn);
                 result.add(data);
             } while (cursor.moveToNext());
@@ -135,18 +138,15 @@ public class MainActivity extends Activity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem iItem)
-    {
-        switch (iItem.getItemId())
-        {
+    public boolean onOptionsItemSelected(android.view.MenuItem iItem) {
+        switch (iItem.getItemId()) {
             case R.id.delete_menu_item:
                 //set the text as deleting
                 iItem.setTitle(R.string.deleting_label);
@@ -163,33 +163,33 @@ public class MainActivity extends Activity
                 mcvAdapter.notifyDataSetChanged();
                 //handled
 
-//                iItem.setChecked(true);
-//                iDelete.setIcon(R.drawable.ic_done_50);
-//                iItem.setTitle(R.string.deleted_label);
                 return true;
             case R.id.upload_menu_item:
-//                //get google account credentials and store to member variable
-//                mgacCredential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
-//                //get a list of all the accounts on the device
-//                Account[] myAccounts = AccountManager.get(this).getAccounts();
-//                //for each account
-//                for(int i = 0; i < myAccounts.length; i++)
-//                {
-//                    //if the account type is google
-//                    if (myAccounts[i].type.equals("com.google"))
-//                        //set this as the selected Account
-//                        mgacCredential.setSelectedAccountName(myAccounts[i].name);
-//                }
-//                //get the drive service
-//                mdService = getDriveService(mgacCredential);
-//                //save the selected item to google drive
-//                saveFileToDrive(mlsPaths.get(iPosition));
+
+                if (mcmCon.getActiveNetworkInfo().isConnected())
+                {
+//                    //get google account credentials and store to member variable
+//                    mgacCredential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
+//                    //get a list of all the accounts on the device
+//                    Account[] myAccounts = AccountManager.get(this).getAccounts();
+//                    //for each account
+//                    for (int i = 0; i < myAccounts.length; i++) {
+//                        //if the account type is google
+//                        if (myAccounts[i].type.equals("com.google"))
+//                            //set this as the selected Account
+//                            mgacCredential.setSelectedAccountName(myAccounts[i].name);
+//                    }
+//                    //get the drive service
+//                    mdService = getDriveService(mgacCredential);
+//                    //save the selected item to google drive
+//                    saveFileToDrive(mlsPaths.get(iPosition));
+                }
+
                 return true;
 
             case R.id.uploadphone_menu_item:
 
-                if (mcmCon.getActiveNetworkInfo().isConnected())
-                {
+                if (mcmCon.getActiveNetworkInfo().isConnected()) {
 
                 }
 
@@ -198,54 +198,46 @@ public class MainActivity extends Activity
             default:
                 return super.onOptionsItemSelected(iItem);
         }
-    };
+    }
+
+    ;
 
 
-//    private void saveFileToDrive(String sPath)
-//    {
-//        final String msPath = sPath;
-//
-//        Thread t = new Thread(new Runnable()
-//        {
-//            @Override
-//            public void run() {
-//                try
-//                {
-//                    // File's binary content
-//                    java.io.File fImage = new java.io.File(msPath);
-//                    FileContent fcContent = new FileContent("image/jpeg", fImage);
-//
-//                    // File's metadata.
-//                    File gdfBody = new File();
-//                    gdfBody.setTitle(fImage.getName());
-//                    gdfBody.setMimeType("image/jpeg");
-//
-//                    File gdfFile = mdService.files().insert(gdfBody, fcContent).execute();
-//                    if (gdfFile != null)
-//                        Log.d("GlassShareUploadTask", "Uploaded");
-//                }
-//                catch (UserRecoverableAuthIOException e)
-//                {
-//                    Log.d("GlassShareUploadTask", e.toString());
-////                    startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
-//                }
-//                catch (IOException e)
-//                {
-//                    Log.d("GlassShareUploadTask", e.toString());
-////                    e.printStackTrace();
-//                }
-//                catch (Exception e)
-//                {
-//                    Log.d("GlassShareUploadTask", e.toString());
-//                }
-//            }
-//        });
-//        t.start();
-//
-//    }
-//
-//    private Drive getDriveService(GoogleAccountCredential credential)
-//    {
-//        return new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential).build();
-//    }
+    private void saveFileToDrive(String sPath) {
+        final String msPath = sPath;
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // File's binary content
+                    java.io.File fImage = new java.io.File(msPath);
+                    FileContent fcContent = new FileContent("image/jpeg", fImage);
+
+                    // File's metadata.
+                    File gdfBody = new File();
+                    gdfBody.setTitle(fImage.getName());
+                    gdfBody.setMimeType("image/jpeg");
+
+                    File gdfFile = mdService.files().insert(gdfBody, fcContent).execute();
+                    if (gdfFile != null)
+                        Log.d("GlassShareUploadTask", "Uploaded");
+                } catch (UserRecoverableAuthIOException e) {
+                    Log.d("GlassShareUploadTask", e.toString());
+//                    startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+                } catch (IOException e) {
+                    Log.d("GlassShareUploadTask", e.toString());
+//                    e.printStackTrace();
+                } catch (Exception e) {
+                    Log.d("GlassShareUploadTask", e.toString());
+                }
+            }
+        });
+        t.start();
+
+    }
+
+    private Drive getDriveService(GoogleAccountCredential credential) {
+        return new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential).build();
+    }
 }
