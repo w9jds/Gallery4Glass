073d506a9d0889fe81f17c20b02ecf9bda106c74
Jeremy Shore <xboxmusicn3rd@gmail.com>
2014-02-28 00:36:59 -0600

    Rebuilt project without Gradle
    - Now uses OpenCV as a dependency
    - Filters rebuilt to use OpenCV (much faster)
    - Fixed saving using a media scanner (known bug, creates two files)
    - Added in the Invert and the Sharpen effects.
    - BUG: Seems to create two files on save. Need to find out how to get
    rid of the random numbers one…

---
+++ src/com/w9jds/gallery4glass/Classes/StorageApplication.java
@@ -0,0 +1,22 @@
+package com.w9jds.gallery4glass.Classes;
+
+import android.app.Application;
+
+/**
+ * Created by w9jds on 12/15/13.
+ */
+public class StorageApplication extends Application
+{
+    private StorageService mStorageService;
+
+    public StorageApplication() {}
+
+    public StorageService getStorageService()
+    {
+        if (mStorageService == null)
+            mStorageService = new StorageService(this);
+
+        return mStorageService;
+    }
+
+}
