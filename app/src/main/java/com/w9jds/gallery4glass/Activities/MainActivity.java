19498eccc2eae782f89976139141eb18883116e2
Jeremy Shore <xboxmusicn3rd@gmail.com>
2015-08-02 23:29:28 -0500

    Started project from scratch
    - Migrated over a Cursor Adapter from base Android for use with the Gallery Images
    - Use Glide/Picasso for loading images instead of handling async, placeholder, and cropping all by hand
    - Much faster loading and better user experiance
    - Code is also much cleaner, thank god I learned something ;)

--- app/src/main/java/com/w9jds/gallery4glass/Activities/MainActivity.java
+++ app/src/main/java/com/w9jds/gallery4glass/Activities/MainActivity.java
@@ -1,260 +1,207 @@
 package com.w9jds.gallery4glass.Activities;
 
-import android.animation.Animator;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
-import android.gesture.Gesture;
-import android.media.AudioManager;
-import android.net.ConnectivityManager;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.provider.MediaStore;
-import android.view.GestureDetector;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardScrollView;
 import com.w9jds.gallery4glass.Adapters.GalleryAdapter;
-import com.w9jds.gallery4glass.Classes.Paths;
-import com.w9jds.gallery4glass.Classes.SingleMediaScanner;
 import com.w9jds.gallery4glass.R;
-import com.w9jds.gdk_progress_widget.SliderView;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class MainActivity extends Activity
-{
-    private final String CAMERA_IMAGE_BUCKET_NAME = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + "/Camera";
-    private final String CAMERA_IMAGE_BUCKET_ID = getBucketId(CAMERA_IMAGE_BUCKET_NAME);
+public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /**
+     * {@link CardScrollView} to use as the main content view.
+     */
+    private CardScrollView mCardScroller;
 
     private ConnectivityManager mConnectionManager;
+    private GalleryAdapter mGalleryAdapter;
     private AudioManager mAudioManager;
 
-    //cardscrollview
-    private CardScrollView mCardsView;
+    public static int COLUMN_DATA = 1;
+    public static int COLUMN_DATE = 2;
 
-
-    //custom adapter
-    private GalleryAdapter mGalleryAdapter;
-    //custom object
-    private Paths mPaths = new Paths();
-
+    private View mView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
 
         mConnectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        CreatePictureView();
-    }
+        mCardScroller = new CardScrollView(this);
 
-//    @Override
-//    public void onStart()
-//    {
-//        super.onStart();
-//        // The rest of your onStart() code.
-//        EasyTracker.getInstance(this).activityStart(this);  // Add this method.
-//    }
-//
-//    @Override
-//    public void onStop()
-//    {
-//        super.onStop();
-//        // The rest of your onStop() code.
-//        EasyTracker.getInstance(this).activityStop(this);  // Add this method.
-//    }
-
-    @Override
-    protected void onDestroy()
-    {
-//        unbindService();
-        super.onDestroy();
-    }
-
-    private void CreatePictureView()
-    {
-        //get all the images from the camera folder (paths)
-        mPaths.setImagePaths(getCameraImages());
-        //sort the paths of pictures
-//        sortPaths(mcpPaths.getImagePaths());
-        Collections.reverse(mPaths.getImagePaths());
-
-        //create a new card scroll viewer for this context
-        mCardsView = new CardScrollView(this);
         //create a new adapter for the scroll viewer
-        mGalleryAdapter = new GalleryAdapter(this, mPaths.getImagePaths());
+        mGalleryAdapter = new GalleryAdapter(this, null, 0);
         //set this adapter as the adapter for the scroll viewer
-        mCardsView.setAdapter(mGalleryAdapter);
+        mCardScroller.setAdapter(mGalleryAdapter);
         //activate this scroll viewer
-        mCardsView.activate();
+        mCardScroller.activate();
         //add a listener to the scroll viewer that is fired when an item is clicked
-        mCardsView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        mCardScroller.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 mAudioManager.playSoundEffect(Sounds.TAP);
-                //save the card index that was selected
-                mPaths.setMainPosition(position);
                 //open the menu
                 openOptionsMenu();
             }
         });
 
         //set the view of this activity
-        setContentView(mCardsView);
+        setContentView(mCardScroller);
+        getLoaderManager().initLoader(0, null, this);
     }
 
-    public String getBucketId(String path)
-    {
-        return String.valueOf(path.toLowerCase().hashCode());
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCardScroller.activate();
     }
 
-    /***
-     * Get all the image file paths on this device (from the camera folder)
-     * @return an arraylist of all the file paths
-     */
-    public ArrayList<String> getCameraImages()
-    {
-        final String[] saProj = {MediaStore.Images.Media.DATA};
-        final String sSelection = MediaStore.Images.Media.BUCKET_ID + " = ?";
-        final String[] saSelectionArgs = { CAMERA_IMAGE_BUCKET_ID };
-        final Cursor cCursor = this.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, saProj, sSelection, saSelectionArgs, null);
-        ArrayList<String> result = new ArrayList<String>(cCursor.getCount());
+    @Override
+    protected void onPause() {
+        mCardScroller.deactivate();
+        super.onPause();
+    }
 
-        if (cCursor.moveToFirst())
-        {
-            final int dataColumn = cCursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            do
-            {
-                final String data = cCursor.getString(dataColumn);
-                result.add(data);
-            }
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Get relevant columns for use later.
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATA,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.MEDIA_TYPE,
+                MediaStore.Files.FileColumns.MIME_TYPE,
+                MediaStore.Files.FileColumns.TITLE
+        };
 
-            while (cCursor.moveToNext());
-        }
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+//                + " OR "
+//                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+//                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
-        cCursor.close();
-        return result;
+        Uri queryUri = MediaStore.Files.getContentUri("external");
+
+
+        return new CursorLoader(this, queryUri, projection, selection,
+                null, MediaStore.Files.FileColumns.DATE_ADDED + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mGalleryAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mGalleryAdapter.swapCursor(null);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.gallery, menu);
         return true;
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (resultCode == RESULT_OK)
-        {
-            switch(requestCode)
-            {
-                case 1:
-                    CreatePictureView();
-                    break;
-            }
-        }
-    }
-
     @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem iItem)
+    public boolean onOptionsItemSelected(android.view.MenuItem menuItem)
     {
-        SliderView svProgress;
 
-        switch (iItem.getItemId())
+        switch (menuItem.getItemId())
         {
-            case R.id.vignette_menu_item:
-                Intent iVignette = new Intent(this, VignetteActivity.class);
-                iVignette.putExtra("PathsObject", mPaths);
-                startActivityForResult(iVignette, 1);
-                return true;
-
-            case R.id.effects_menu_item:
-                Intent iEffects = new Intent(this, EffectActivity.class);
-                iEffects.putExtra("PathsObject", mPaths);
-                startActivityForResult(iEffects, 1);
-                return true;
-
+//            case R.id.vignette_menu_item:
+//                Intent iVignette = new Intent(this, VignetteActivity.class);
+//                iVignette.putExtra("PathsObject", mPaths);
+//                startActivityForResult(iVignette, 1);
+//                return true;
+//
+//            case R.id.effects_menu_item:
+//                Intent iEffects = new Intent(this, EffectActivity.class);
+//                iEffects.putExtra("PathsObject", mPaths);
+//                startActivityForResult(iEffects, 1);
+//                return true;
+//
             case R.id.delete_menu_item:
-                //set the text as deleting
-                setContentView(R.layout.menu_layout);
-                ((ImageView)findViewById(R.id.icon)).setImageResource(R.drawable.ic_delete_50);
-                ((TextView)findViewById(R.id.label)).setText(getString(R.string.deleting_label));
 
-                svProgress = (SliderView)findViewById(R.id.slider);
-                svProgress.startProgress(1000, new Animator.AnimatorListener()
-                {
-                    @Override
-                    public void onAnimationStart(Animator animation)
-                    {
 
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation)
-                    {
-                        //pull the file from the path of the selected item
-                        java.io.File fPic = new java.io.File(mPaths.getCurrentPositionPath());
-                        //delete the image
-                        fPic.delete();
-                        //refresh the folder
-                        new SingleMediaScanner(getApplicationContext(), fPic);
-                        //remove the selected item from the list of images
-                        mPaths.removeCurrentPositionPath();
-                        //let the adapter know that the list of images has changed
-                        mGalleryAdapter.notifyDataSetChanged();
-                        //handled
-
-                        setContentView(R.layout.menu_layout);
-                        ((ImageView)findViewById(R.id.icon)).setImageResource(R.drawable.ic_done_50);
-                        ((TextView)findViewById(R.id.label)).setText(getString(R.string.deleted_label));
-
-                        mAudioManager.playSoundEffect(Sounds.SUCCESS);
-
-                        new Handler().postDelayed(new Runnable()
-                        {
-                            public void run()
-                            {
-                                CreatePictureView();
-                            }
-                        }, 1000);
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation)
-                    {
-                        CreatePictureView();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation)
-                    {
-
-                    }
-                });
-                return true;
+//                //set the text as deleting
+//                setContentView(R.layout.menu_layout);
+//                ((ImageView)findViewById(R.id.icon)).setImageResource(R.drawable.ic_delete_50);
+//                ((TextView)findViewById(R.id.label)).setText(getString(R.string.deleting_label));
+//
+//                svProgress = (SliderView)findViewById(R.id.slider);
+//                svProgress.startProgress(1000, new Animator.AnimatorListener()
+//                {
+//                    @Override
+//                    public void onAnimationStart(Animator animation)
+//                    {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animator animation)
+//                    {
+//                        //pull the file from the path of the selected item
+//                        java.io.File fPic = new java.io.File(mPaths.getCurrentPositionPath());
+//                        //delete the image
+//                        fPic.delete();
+//                        //refresh the folder
+//                        new SingleMediaScanner(getApplicationContext(), fPic);
+//                        //remove the selected item from the list of images
+//                        mPaths.removeCurrentPositionPath();
+//                        //let the adapter know that the list of images has changed
+//                        mGalleryAdapter.notifyDataSetChanged();
+//                        //handled
+//
+//                        setContentView(R.layout.menu_layout);
+//                        ((ImageView)findViewById(R.id.icon)).setImageResource(R.drawable.ic_done_50);
+//                        ((TextView)findViewById(R.id.label)).setText(getString(R.string.deleted_label));
+//
+//                        mAudioManager.playSoundEffect(Sounds.SUCCESS);
+//
+//                        new Handler().postDelayed(new Runnable()
+//                        {
+//                            public void run()
+//                            {
+//                                CreatePictureView();
+//                            }
+//                        }, 1000);
+//                    }
+//
+//                    @Override
+//                    public void onAnimationCancel(Animator animation)
+//                    {
+//                        CreatePictureView();
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animator animation)
+//                    {
+//
+//                    }
+//                });
+//                return true;
 
             default:
-                return super.onOptionsItemSelected(iItem);
+                return super.onOptionsItemSelected(menuItem);
         }
     }
 }
-
-
-
--- app/src/main/java/com/w9jds/gallery4glass/Adapters/GalleryAdapter.java
+++ app/src/main/java/com/w9jds/gallery4glass/Adapters/GalleryAdapter.java
@@ -1,248 +1,77 @@
 package com.w9jds.gallery4glass.Adapters;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
+import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.glass.widget.CardScrollAdapter;
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+import com.w9jds.gallery4glass.Activities.MainActivity;
 import com.w9jds.gallery4glass.R;
+import com.w9jds.gallery4glass.Widgets.CardCursorAdapter;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class GalleryAdapter extends CardScrollAdapter
-{
-    private Context mcContext;
-    private ArrayList<String> mlsPaths;
-    private Bitmap mPlaceHolderBitmap;
+/**
+ * Created by JeremyShore on 4/5/15.
+ */
+public class GalleryAdapter extends CardCursorAdapter {
 
-    public GalleryAdapter(Context cContext, ArrayList<String> alsPaths)
-    {
-        mcContext = cContext;
-        mlsPaths = alsPaths;
-        mPlaceHolderBitmap = BitmapFactory.decodeResource(mcContext.getResources(), R.drawable.ic_placeholder_photo_150);
+    public GalleryAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    public static class ViewHolder {
+        ImageView picture;
+        TextView timestamp;
     }
 
     @Override
-    public int getCount()
-    {
-        return mlsPaths.size();
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        final int layout = getItemViewType(cursor.getPosition());
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View convertView = layoutInflater.inflate(layout, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.picture = (ImageView) convertView.findViewById(R.id.cardImage);
+        viewHolder.timestamp = (TextView) convertView.findViewById(R.id.datestamp);
+        convertView.setTag(viewHolder);
+
+        return convertView;
     }
 
     @Override
-    public Object getItem(int iPosition)
-    {
-        return mlsPaths.get(iPosition);
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder)view.getTag();
+
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd");
+//        Date added = new Date(cursor.getLong(MainActivity.COLUMN_DATE));
+//        viewHolder.timestamp.setText(dateFormat.format(added));
+
+        Glide.with(context)
+                .load("file://" + cursor.getString(MainActivity.COLUMN_DATA))
+                .centerCrop()
+                .placeholder(R.drawable.ic_placeholder_photo_150)
+                .crossFade()
+                .thumbnail(0.2f)
+                .into(viewHolder.picture);
+
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        View vCard = null;
-        LayoutInflater inflater;
-        TextView tvText;
-
-        if (mlsPaths.get(position).contains("/"))
-        {
-            inflater = (LayoutInflater) mcContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            vCard = inflater.inflate(R.layout.imagecard_layout, parent, false);
-
-            //get the imageview we are going to populate
-            ImageView imageView = (ImageView) vCard.findViewById(R.id.cardImage);
-
-
-            if (cancelPotentialWork(position, imageView))
-            {
-                final BitmapWorkerTask task = new BitmapWorkerTask(imageView, mlsPaths, position);
-
-                final AsyncDrawable asyncDrawable = new AsyncDrawable(mcContext.getResources(), mPlaceHolderBitmap, task);
-                imageView.setScaleType(ImageView.ScaleType.CENTER);
-                imageView.setImageDrawable(asyncDrawable);
-
-                task.execute();
-            }
-        }
-        else
-        {
-            if (mlsPaths.get(position) == "Text")
-            {
-                inflater = (LayoutInflater) mcContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                vCard = inflater.inflate(R.layout.menu_layout, parent, false);
-
-                //get the textview we are going to populate
-                tvText = (TextView) vCard.findViewById(R.id.label);
-                //get the icon we are going to set
-                ImageView ivImage = (ImageView) vCard.findViewById(R.id.icon);
-
-                tvText.setText(mlsPaths.get(position));
-                ivImage.setImageResource(R.drawable.ic_document_50);
-            }
-            else
-            {
-                inflater = (LayoutInflater) mcContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                vCard = inflater.inflate(R.layout.textcard_layout, parent, false);
-
-                //get the textview we are going to populate
-                tvText = (TextView) vCard.findViewById(R.id.cardText);
-
-                tvText.setText(mlsPaths.get(position));
-            }
-        }
-
-        return vCard;
+    public int getItemViewType(int position) {
+        return R.layout.image_layout;
     }
 
     @Override
-    public int getPosition(Object o)
-    {
+    public int getPosition(Object o) {
         return 0;
-    }
-
-    public static boolean cancelPotentialWork(int iPosition, ImageView ivCard)
-    {
-        final BitmapWorkerTask bwtTask = getBitmapWorkerTask(ivCard);
-
-        if (bwtTask != null)
-        {
-            final int position = bwtTask.miPosition;
-
-            if (position != iPosition)
-                // Cancel previous task
-                bwtTask.cancel(true);
-            else
-                // The same work is already in progress
-                return false;
-        }
-        // No task associated with the ImageView, or an existing task was cancelled
-        return true;
-    }
-
-    static class AsyncDrawable extends BitmapDrawable
-    {
-        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
-
-        public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bwTask)
-        {
-            super(res, bitmap);
-            bitmapWorkerTaskReference = new WeakReference<BitmapWorkerTask>(bwTask);
-        }
-
-        public BitmapWorkerTask getBitmapWorkerTask() {
-            return bitmapWorkerTaskReference.get();
-        }
-    }
-
-    private static BitmapWorkerTask getBitmapWorkerTask(ImageView ivCard)
-    {
-        if (ivCard != null)
-        {
-            final Drawable drawable = ivCard.getDrawable();
-            if (drawable instanceof AsyncDrawable)
-            {
-                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
-                return asyncDrawable.getBitmapWorkerTask();
-            }
-        }
-        return null;
-    }
-
-    public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap>
-    {
-        private final ArrayList<String> mlsPaths;
-        private final WeakReference<ImageView> mivCard;
-        private final int miPosition;
-
-        public BitmapWorkerTask(ImageView ivCard, ArrayList<String> lsPaths, int iPosition)
-        {
-            // Use a WeakReference to ensure the ImageView can be garbage collected
-            mivCard = new WeakReference<ImageView>(ivCard);
-            miPosition = iPosition;
-            mlsPaths = lsPaths;
-
-        }
-
-        // Decode image in background.
-        @Override
-        protected Bitmap doInBackground(Integer... params)
-        {
-            //create new options for bitmap import
-            BitmapFactory.Options bfoOptions = new BitmapFactory.Options();
-            //set it so you only get bounds (no pixels)
-            bfoOptions.inJustDecodeBounds = true;
-            //pull the info from the file
-            BitmapFactory.decodeFile(mlsPaths.get(miPosition), bfoOptions);
-
-            if (bfoOptions.outWidth > 640)
-            {
-                //turn off the bounds only so you get the pixels this time
-                bfoOptions.inJustDecodeBounds = false;
-                //calculate the sample size and set it in the options
-                bfoOptions.inSampleSize = calculateInSampleSize(bfoOptions, 640, 360);
-
-                Bitmap bImage = BitmapFactory.decodeFile(mlsPaths.get(miPosition), bfoOptions);
-
-                double dRatio = ((double) bImage.getWidth()) / 640;
-                return Bitmap.createScaledBitmap(bImage, 640, (int) Math.round(bImage.getHeight() / dRatio), true);
-            }
-
-            else
-                return BitmapFactory.decodeFile(mlsPaths.get(miPosition));
-
-        }
-
-        public int calculateInSampleSize(BitmapFactory.Options bfoOptions, int iReqWidth, int iReqHeight)
-        {
-            //pull out the images height
-            final int height = bfoOptions.outHeight;
-            //pull out the images width
-            final int width = bfoOptions.outWidth;
-            //set the samle size to 1 for initialization
-            int inSampleSize = 1;
-
-            //if te height or the width of the image is greater than the requested sizes
-            if (height > iReqHeight || width > iReqWidth)
-            {
-                //set the half dimensions for the image
-                final int iHalfHeight = height / 2;
-                final int iHalfWidth = width / 2;
-
-                //white both half dimensions divided by the sample size are still greater than the requested dimensions
-                while ((iHalfHeight / inSampleSize) > iReqHeight && (iHalfWidth / inSampleSize) > iReqWidth)
-                    //multiply the sample size by 2
-                    inSampleSize *= 2;
-            }
-
-            //once the loop is done return the sample size
-            return inSampleSize;
-        }
-
-        // Once complete, see if ImageView is still around and set bitmap.
-        @Override
-        protected void onPostExecute(Bitmap bPic)
-        {
-            if (isCancelled())
-                bPic = null;
-
-            if (mivCard != null && bPic != null)
-            {
-                final ImageView ivCard = mivCard.get();
-                final BitmapWorkerTask bwTask = getBitmapWorkerTask(ivCard);
-
-                if (this == bwTask && ivCard != null)
-                {
-                    ivCard.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                    ivCard.setImageBitmap(bPic);
-                }
-            }
-        }
     }
 }
